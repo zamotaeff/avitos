42fb627c34e41f41f6c93adff4e2dc49b2844652
levi.german <levi.german@yandex.ru>
2022-10-23 00:41:22 +0300

    Move folder datasets

--- project/helpers.py
+++ project/helpers.py
@@ -13,9 +13,12 @@ FILE_JSON_USERS = '../datasets/users.json'
 
 
 def convert_csv_to_json(csv_file, json_file):
+    """
+
+    """
     with open(csv_file, 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.DictReader(csvfile)
         rows = list(csv_reader)
 
-    with open(json_file, 'w', encoding='utf-8') as f:
-        json.dump(rows, f, ensure_ascii=False)
+    with open(json_file, 'w', encoding='utf-8') as file:
+        json.dump(rows, file, ensure_ascii=False)
