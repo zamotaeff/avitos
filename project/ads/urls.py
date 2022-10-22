9b82a93f21fa63c7f4039063e3bd548401d1ca86
levi.german <levi.german@yandex.ru>
2022-10-23 02:33:18 +0300

    Create urls.py for ads app

---
+++ project/ads/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from ads.views import (
+    index_route,
+    AdListView,
+    AdCreateView,
+    AdDeleteView,
+    AdUpdateView,
+    AdUploadImageView,
+    CategoryListView,
+    CategoryCreateView,
+    AdDetailView,
+    CategoryDetailView,
+    CategoryDeleteView,
+    CategoryUpdateView
+)
+
+urlpatterns = [
+    path('', index_route),
+    path('ad/', AdListView.as_view()),
+    path('ad/create/', AdCreateView.as_view()),
+    path('ad/<int:pk>/', AdDetailView.as_view()),
+    path('ad/<int:pk>/delete/', AdDeleteView.as_view()),
+    path('ad/<int:pk>/update/', AdUpdateView.as_view()),
+    path('ad/<int:pk>/upload_image/', AdUploadImageView.as_view()),
+    path('cat/', CategoryListView.as_view()),
+    path('cat/create/', CategoryCreateView.as_view()),
+    path('cat/<int:pk>/', CategoryDetailView.as_view()),
+    path('cat/<int:pk>/delete/', CategoryDeleteView.as_view()),
+    path('cat/<int:pk>/update/', CategoryUpdateView.as_view()),
+]
--- project/avito/urls.py
+++ project/avito/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from ads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.root),
-    path('cat/', views.CategoryView.as_view()),
-    path('cat/<int:pk>', views.CategoryDetailView.as_view()),
-    path('ad/', views.AdView.as_view()),
-    path('ad/<int:pk>', views.AdDetailView.as_view()),
+    path('', include('ads.urls'))
 ]
