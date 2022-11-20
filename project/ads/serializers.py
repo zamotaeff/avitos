18819945b540b939a8df30d5002becaec5a55e36
zamotaeff <zamotaev.v-a@yandex.ru>
2022-11-20 09:22:16 +0300

    Add serializers.py

--- project/ads/serializers.py
+++ project/ads/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 from ads.models import Ad, Category, Selection
+from ads.validators import not_published
 from users.models import User
 from users.serializers import UserAdSerializer
 
@@ -12,9 +13,19 @@ class AdSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class AdCreateSerializer(serializers.ModelSerializer):
+    is_published = serializers.BooleanField(validators=[not_published])
+
+    class Meta:
+        model = Ad
+        fields = "__all__"
+
+
 class AdListSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', queryset=User.objects.all())
-    category = SlugRelatedField(slug_field='name', queryset=Category.objects.all())
+    author = SlugRelatedField(slug_field='username',
+                              queryset=User.objects.all())
+    category = SlugRelatedField(slug_field='name',
+                                queryset=Category.objects.all())
 
     class Meta:
         model = Ad
@@ -23,24 +34,35 @@ class AdListSerializer(serializers.ModelSerializer):
 
 class AdDetailSerializer(serializers.ModelSerializer):
     author = UserAdSerializer
-    category = SlugRelatedField(slug_field='name', queryset=Category.objects.all())
+    category = SlugRelatedField(slug_field='name',
+                                queryset=Category.objects.all())
 
     class Meta:
         model = Ad
         fields = "__all__"
+
 
 class SelectionListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Selection
         fields = ["id", "name"]
 
+
 class SelectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Selection
         fields = "__all__"
 
+
 class SelectionDetailSerializer(serializers.ModelSerializer):
     items = AdSerializer(many=True)
+
     class Meta:
         model = Selection
         fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
